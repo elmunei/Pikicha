2a06c0038c38a710001f0c3fef66a5bcd302f9a8
Elvis Tapfumanei <elvis@elmunei.net>
2017-05-26 20:16:56 +0200

    Initial Commit

---
+++ pikichaTests/pikichaTests.swift
@@ -0,0 +1,36 @@
+//
+//  pikichaTests.swift
+//  pikichaTests
+//
+//  Created by Elvis Tapfumanei on 2017/05/26.
+//  Copyright © 2017 Elmunei. All rights reserved.
+//
+
+import XCTest
+@testable import pikicha
+
+class pikichaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
